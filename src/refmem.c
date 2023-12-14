ccda6eda697dd19bb9ebe584cdabfd9f745eb6e0
Hektor Einarsson <hektor@einarssons.se>
2023-12-14 02:41:36 +0100

    refactored reference count & implemente overflow handling

--- src/refmem.c
+++ src/refmem.c
@@ -9,9 +9,9 @@ static size_t cascade_limit = 5;
 static Queue *to_be_freed = NULL;
 
 // TODO: gör om till macro
-obj *getCounter(obj *obj_ptr) 
+obj *getCounter(obj *obj_ptr)
 {
-    return ((int *)obj_ptr) - 1;
+    return ((unsigned short *)obj_ptr) - 1;
 }
 
 static void free_from_queue()
@@ -33,7 +33,7 @@ static void free_from_queue()
     }
 }
 
-obj *allocate(size_t bytes, function1_t destructor) 
+obj *allocate(size_t bytes, function1_t destructor)
 {
     int *allocation = calloc(1, (COUNTERSIZE + bytes));
     (*allocation) = 0;
@@ -43,77 +43,82 @@ obj *allocate(size_t bytes, function1_t destructor)
     return &(allocation[1]);
 }
 
-void deallocate(obj *obj_ptr) 
+void deallocate(obj *obj_ptr)
 {
-    int *counterPointer = getCounter(obj_ptr);
+    unsigned short *counterPointer = getCounter(obj_ptr);
     free(counterPointer);
 }
 
-void retain(obj *obj_ptr) 
+void retain(obj *obj_ptr)
 {
-    int *counterPointer = getCounter(obj_ptr);
+    unsigned short *counterPointer = getCounter(obj_ptr);
+    if (*counterPointer == 65535) // Reference count max size
+    {
+        counterPointer = 0;
+        deallocate(obj_ptr);
+    }
     (*counterPointer)++;
 }
 
-static void add_to_free_queue(obj *obj_to_free) 
+static void add_to_free_queue(obj *obj_to_free)
 {
-    if (to_be_freed == NULL) 
+    if (to_be_freed == NULL)
     {
         to_be_freed = create_queue();
-    } 
+    }
     enqueue(to_be_freed, obj_to_free);
 }
 
-void release(obj *obj_ptr) 
+void release(obj *obj_ptr)
 {
-    int *counterPointer = getCounter(obj_ptr);
+    unsigned short *counterPointer = getCounter(obj_ptr);
     (*counterPointer)--;
 
-    if ((*counterPointer) <= 0) 
+    if ((*counterPointer) <= 0)
     {
         add_to_free_queue(obj_ptr);
     }
 }
 
-size_t rc(obj *obj_ptr) 
+unsigned short rc(obj *obj_ptr)
 {
-    int *counterPointer = getCounter(obj_ptr);
+    unsigned short *counterPointer = getCounter(obj_ptr);
     return *counterPointer;
 }
 
-obj *allocate_array(size_t elements, size_t elem_size, function1_t destructor) 
+obj *allocate_array(size_t elements, size_t elem_size, function1_t destructor)
 {
     int *allocation = calloc(1, (COUNTERSIZE + (elements * elem_size)));
     (*allocation) = 0;
     return &(allocation[1]);
 }
 
-void set_cascade_limit(size_t new) 
+void set_cascade_limit(size_t new)
 {
     cascade_limit = new;
 }
 
-size_t get_cascade_limit() 
+size_t get_cascade_limit()
 {
     return cascade_limit;
 }
 
-void cleanup() 
+void cleanup()
 {
     if (to_be_freed == NULL)
     {
         return;
-    } 
-    else 
+    }
+    else
     {
-        while (!is_empty(to_be_freed)) 
+        while (!is_empty(to_be_freed))
         {
             deallocate(dequeue(to_be_freed));
         }
     }
 }
 
-void shutdown() 
+void shutdown()
 {
-    free(to_be_freed); 
+    free(to_be_freed);
 }
